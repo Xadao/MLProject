67012f79748e16af57e0d31e6d99ab4f2b59db56
Xadao <64237097+Xadao@users.noreply.github.com>
2020-05-12 03:30:59 +0200

    First
    First

---
+++ neural_net.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+train_dataset_fp = "hu2222.csv"
+
+column_names = ['Hu1', 'Hu2', 'Hu3', 'Hu4', 'Hu5', 'Hu6', 'Hu7', 'Class']
+
+feature_names = column_names[:-1]
+label_name = column_names[-1]
+
+print("Features: {}".format(feature_names))
+print("Label: {}".format(label_name))
+
+class_names = ['0', '1', '2', '3']
+
+batch_size = 256
+
+train_dataset = tf.data.experimental.make_csv_dataset(
+    train_dataset_fp,
+    batch_size,
+    column_names=column_names,
+    label_name=label_name,
+    num_epochs=1)
+
+features, labels = next(iter(train_dataset))
+
+def pack_features_vector(features, labels):
+  """Pack the features into a single array."""
+  features = tf.stack(list(features.values()), axis=1)
+  return features, labels
+
+train_dataset = train_dataset.map(pack_features_vector)
+
+features, labels = next(iter(train_dataset))
+
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(256, activation=tf.nn.selu, input_shape=(7,)),  # input shape required
+  tf.keras.layers.Dense(128, activation=tf.nn.tanh),
+  tf.keras.layers.Dense(128, activation=tf.nn.tanh),
+  tf.keras.layers.Dense(4,  )
+])
+
+predictions = model(features)
+predictions[:7]
+
+tf.nn.softmax(predictions[:7])
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+def loss(model, x, y, training):
+  # training=training is needed only if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  y_ = model(x, training=training)
+
+  return loss_object(y_true=y, y_pred=y_)
+
+
+l = loss(model, features, labels, training=False)
+
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets, training=True)
+  return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+loss_value, grads = grad(model, features, labels)
+
+print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(),
+                                          loss_value.numpy()))
+
+optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+print("Step: {},         Loss: {}".format(optimizer.iterations.numpy(),
+                                          loss(model, features, labels, training=True).numpy()))
+
+train_loss_results = []
+train_accuracy_results = []
+
+num_epochs =140
+
+for epoch in range(num_epochs):
+  epoch_loss_avg = tf.keras.metrics.Mean()
+  epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+
+  # Training loop 
+  for x, y in train_dataset:
+    # Optimize the model
+    loss_value, grads = grad(model, x, y)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    # Track progress
+    epoch_loss_avg(loss_value)  # Add current batch loss
+    # Compare predicted label to actual label
+    # training=True is needed only if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    epoch_accuracy(y, model(x, training=True))
+
+  # End epoch
+  train_loss_results.append(epoch_loss_avg.result())
+  train_accuracy_results.append(epoch_accuracy.result())
+
+  if epoch % 20 == 0:
+    print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+                                                                epoch_loss_avg.result(),
+                                                                epoch_accuracy.result()))
+
+
+
+fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+fig.suptitle('Training Metrics')
+
+axes[0].set_ylabel("Loss", fontsize=14)
+axes[0].plot(train_loss_results)
+
+axes[1].set_ylabel("Accuracy", fontsize=14)
+axes[1].set_xlabel("Epoch", fontsize=14)
+axes[1].plot(train_accuracy_results)
+plt.show()
+
+
+
+
+
